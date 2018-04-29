14fb7dcabd37388a6094a6347531f77c93bcc6d8
Terry Moore <tmm@mcci.com>
2018-04-28 22:28:30 -0400

    Add test examples

---
+++ examples/header_test_lmic/header_test_lmic.ino
@@ -0,0 +1,24 @@
+/*
+
+Module:  header_test_lmic.ino
+
+Function:
+        Simple compile-test app for Arduino_LoRaWAN_lmic.h
+
+Copyright notice and License:
+        See LICENSE file accompanying this project.
+
+Author:
+        Terry Moore, MCCI Corporation	April 2018
+
+*/
+
+#include <Arduino_LoRaWAN_lmic.h>
+
+void setup()
+	{
+	}
+
+void loop()
+	{
+	}
