958f4a55aecb895c82d65dba023b60014e42531c
Terry Moore <tmm@mcci.com>
2020-01-02 15:24:03 -0500

    Clean up boilerplate comments.

--- src/lib/ttn_au915_netjoin.cpp
+++ src/lib/ttn_au915_netjoin.cpp
@@ -1,5 +1,3 @@
-/* ttn_au915_netjoin.cpp	Mon Aug 07 2017 00:41:05 tmm */
-
 /*
 
 Module:  ttn_au915_netjoin.cpp
@@ -7,27 +5,11 @@ Module:  ttn_au915_netjoin.cpp
 Function:
 	Arduino_LoRaWAN_ttn_au915::NetJoin()
 
-Version:
-	V0.2.4	Mon Aug 07 2017 00:41:05 tmm	Edit level 1
-
 Copyright notice:
-	This file copyright (C) 2017 by
+        See LICENSE file accompanying this project.
 
-		MCCI Corporation
-		3520 Krums Corners Road
-		Ithaca, NY  14850
-
-	An unpublished work.  All rights reserved.
-	
-	This file is proprietary information, and may not be disclosed or
-	copied without the prior permission of MCCI Corporation.
- 
 Author:
 	Terry Moore, MCCI Corporation	August 2017
-
-Revision history:
-   0.2.4  Mon Aug 07 2017 00:41:05  tmm
-	Module created.
 
 */
 
@@ -36,10 +18,7 @@ Revision history:
 
 /****************************************************************************\
 |
-|		Manifest constants & typedefs.
-|
-|	This is strictly for private types and constants which will not 
-|	be exported.
+|	Manifest constants & typedefs.
 |
 \****************************************************************************/
 
@@ -49,23 +28,13 @@ Revision history:
 |
 |	Read-only data.
 |
-|	If program is to be ROM-able, these must all be tagged read-only 
-|	using the ROM storage class; they may be global.
-|
 \****************************************************************************/
 
 
 
 /****************************************************************************\
 |
-|	VARIABLES:
-|
-|	If program is to be ROM-able, these must be initialized
-|	using the BSS keyword.  (This allows for compilers that require
-|	every variable to have an initializer.)  Note that only those 
-|	variables owned by this module should be declared here, using the BSS
-|	keyword; this allows for linkers that dislike multiple declarations
-|	of objects.
+|	Variables.
 |
 \****************************************************************************/
 
