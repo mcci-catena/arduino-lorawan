e2a66b715f70e7a320ffde09f990a911dd5a48f4
Terry Moore <tmm@mcci.com>
2020-02-09 21:31:28 -0500

    Add Senra

---
+++ src/lib/senra_base_netjoin.cpp
@@ -0,0 +1,44 @@
+/*
+
+Module:  senra_base_netjoin.cpp
+
+Function:
+    Arduino_LoRaWAN_Senra_base::NetJoin()
+
+Copyright notice:
+    See LICENSE file accompanying this project.
+
+Author:
+    Terry Moore, MCCI Corporation	February 2020
+
+*/
+
+#include <Arduino_LoRaWAN_Senra.h>
+#include <Arduino_LoRaWAN_lmic.h>
+
+/****************************************************************************\
+|
+|	Manifest constants & typedefs.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Read-only data.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Variables.
+|
+\****************************************************************************/
+
+
+void Arduino_LoRaWAN_Senra_base::NetJoin()
+    {
+    }
