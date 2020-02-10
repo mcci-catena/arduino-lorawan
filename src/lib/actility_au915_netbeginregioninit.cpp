991bf0dc4d40794e58cb78ffc18f4b813191e09d
Terry Moore <tmm@mcci.com>
2020-02-09 19:18:38 -0500

    Add Actility support

---
+++ src/lib/actility_au915_netbeginregioninit.cpp
@@ -0,0 +1,49 @@
+/*
+
+Module:  actility_au915_netbeginregioninit.cpp
+
+Function:
+    Arduino_LoRaWAN_Actility_au915::NetBeginRegionInit()
+
+Copyright notice:
+    See LICENSE file accompanying this project.
+
+Author:
+    Terry Moore, MCCI Corporation	February 2020
+
+*/
+
+#include <Arduino_LoRaWAN_Actility.h>
+#include <Arduino_LoRaWAN_lmic.h>
+
+/****************************************************************************\
+|
+|	Manifest constants & typedefs.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Read-only data.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Variables.
+|
+\****************************************************************************/
+
+// protected virtual
+void Arduino_LoRaWAN_Actility_au915::NetBeginRegionInit()
+    {
+    // Select SubBand prejoin -- saves power for joining
+    // This is specific to the AU915 bandplan.
+# if defined(ARDUINO_LMIC_CFG_SUBBAND) && ARDUINO_LMIC_CFG_SUBBAND != -1
+    LMIC_selectSubBand(ARDUINO_LMIC_CFG_SUBBAND);
+# endif // defined(ARDUINO_LMIC_CFG_SUBBAND) && ARDUINO_LMIC_CFG_SUBBAND != -1
+    }
---
+++ src/lib/actility_au915_netjoin.cpp
@@ -0,0 +1,45 @@
+/*
+
+Module:  actility_au915_netjoin.cpp
+
+Function:
+    Arduino_LoRaWAN_Actility_au915::NetJoin()
+
+Copyright notice:
+    See LICENSE file accompanying this project.
+
+Author:
+    Terry Moore, MCCI Corporation	February 2020
+
+*/
+
+#include <Arduino_LoRaWAN_Actility.h>
+#include <Arduino_LoRaWAN_lmic.h>
+
+/****************************************************************************\
+|
+|	Manifest constants & typedefs.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Read-only data.
+|
+\****************************************************************************/
+
+
+
+/****************************************************************************\
+|
+|	Variables.
+|
+\****************************************************************************/
+
+void Arduino_LoRaWAN_Actility_au915::NetJoin()
+    {
+    // do the common work.
+    this->Super::NetJoin();
+    }
