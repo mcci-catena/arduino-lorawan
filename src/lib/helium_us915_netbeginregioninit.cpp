16a804b1bdb1cd06ef5d45d20d7b50adc17af342
Dhinesh Kumar Pitchai <dhineshkumar@mcci.com>
2020-05-08 00:51:25 +0530

    changed the SubBand of Helium US 915 to SubBand_2

--- src/lib/helium_us915_netbeginregioninit.cpp
+++ src/lib/helium_us915_netbeginregioninit.cpp
@@ -46,7 +46,7 @@ void Arduino_LoRaWAN_Helium_us915::NetBeginRegionInit()
     // Helium is an 8-channel network on subband 6 of [0..7]. Pre-join
     // we therefore want to limit to probing channels 48~55 / 70.
     //
-    cLMIC::SelectSubBand(cLMIC::SubBand::SubBand_7);
+    cLMIC::SelectSubBand(cLMIC::SubBand::SubBand_2);
 
     //
     // Set data rate and transmit power -- these ought to
