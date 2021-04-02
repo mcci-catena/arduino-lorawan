8f63546b9ce752a7866571a8a173e0f7c8a7723f
Terry Moore <tmm@mcci.com>
2021-04-02 16:29:22 -0400

    Step version to 0.9.0-11

--- src/Arduino_LoRaWAN.h
+++ src/Arduino_LoRaWAN.h
@@ -34,7 +34,7 @@ Author:
 ///     \ref ARDUINO_LORAWAN_VERSION_COMPARE_LT() to compare relative versions.
 ///
 #define ARDUINO_LORAWAN_VERSION \
-        ARDUINO_LORAWAN_VERSION_CALC(0, 9, 0, 10) /* v0.9.0-10 */
+        ARDUINO_LORAWAN_VERSION_CALC(0, 9, 0, 11) /* v0.9.0-11 */
 
 #define ARDUINO_LORAWAN_VERSION_GET_MAJOR(v)    \
         (((v) >> 24u) & 0xFFu)
